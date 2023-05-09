4a8046b8698a3a3b8b51adf018b0ba2eef7c680e
DucManh2103 <124852371+DucManh2103@users.noreply.github.com>
2023-05-09 10:43:36 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 package bai1buoi3;
 
 public class Main {
+  public static void main(String[] args) {
+	    // Tạo đối tượng Order
+	    Order order = new Order(1, LocalDate.of(2023, 5, 9));
+
+	    // Thêm các sản phẩm vào đơn hàng
+	    order.addLineItem(new Product("sp4", "Nước tương", 8000), 10);
+	    order.addLineItem(new Product("sp1", "Gạo", 18000), 5);
+	    order.addLineItem(new Product("sp4", "Đường", 10000), 1);
+	    order.addLineItem(new Product("sp1", "Gạo", 18000), 1);
+
+	    // In kết quả ra màn hình
+	    System.out.println(order.toString());
+	}
 
 }
